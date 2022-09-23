22448ab363a429814bbbf545f49dea6b6e6898eb
christian-bayata <franksagie1@gmail.com>
2022-09-23 16:32:43 +0100

    Setup end-to-end test with jest

--- src/tests/integration/userController.test.ts
+++ src/tests/integration/userController.test.ts
@@ -1,10 +1,13 @@
 import { setup as setupApplication } from "../../app";
 export const app = setupApplication();
 import request from "supertest";
+import models from "../../models/index";
+import crypto from "crypto";
 
 describe("User Resource", () => {
   let server: any;
   let port = process.env.TEST_PORT;
+  let BASE_URL = "/api/users";
 
   const setupTestServer = () => {
     return app.listen(port, () => console.log(`Listening on testing port ${port}`));
@@ -17,6 +20,8 @@ describe("User Resource", () => {
 
   afterEach(async () => {
     server.close();
+    await models.Code.truncate({ force: true });
+    await models.User.truncate({ force: true });
   });
 
   describe("Health Check", () => {
@@ -27,5 +32,15 @@ describe("User Resource", () => {
     });
   });
 
-  // describe("User should get verification code ");
+  describe("User Verification Code", () => {
+    it("should fail if user email already exists", async () => {
+      await models.User.create({ firstName: "test_first", lastName: "test_last", email: "test_email@gmail.com", password: "password", reputation: 0, address: "test_address", phone: "098765432345", city: "test_city", state: "test_state", userTypes: "user" });
+      const createVerCode = await models.Code.create({ email: "test_email@gmail.com", code: crypto.randomBytes(3).toString("hex").toUpperCase() });
+
+      const response = await request(server).post(`${BASE_URL}/get-verification-code`).send(createVerCode);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toMatch(/have an account/i);
+      expect(response.body.success).toBeFalsy();
+    });
+  });
 });
